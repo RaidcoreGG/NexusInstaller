f7300de3c0ffe4eadd6b3665914602f72f453e74
Delta <46466697+DeltaGW2@users.noreply.github.com>
2023-12-30 20:30:26 +0100

    remove unused references

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NexusInstaller
